798cf15b337df62fb7ad57f9b85fb7c50d026f8c
shubhamarman13 <shubhamarman13@gmail.com>
2024-01-12 00:36:28 +0530

    update java

---
+++ first_java_program.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class first_java_program {
+ 
+    public static void main(String[] args) {
+        System.out.println("hi i am shubham amran");
+        Scanner take = new Scanner(System.in);
+        String x= take.nextLine();
+        System.out.println(x);
+    }
+    // System.out.println("i am shubham");
+}
