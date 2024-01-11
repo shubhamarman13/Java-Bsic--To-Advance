263e3f0bbe663bd79e02e68819f2d62874599a3b
shubhamarman13 <shubhamarman13@gmail.com>
2024-01-12 01:11:37 +0530

    update

--- input_in.java
+++ input_in.java
@@ -5,8 +5,13 @@ public class input_in {
      public static void main(String[] args) {
         System.out.println("hi i am shubham amran");
         Scanner take = new Scanner(System.in);
+        // taking String as input
         String x= take.nextLine();
         System.out.println(x);
+
+        // taking Integer as input
+        int y= take.nextInt();
+        System.out.println(y);    
     }
-    // System.out.println("i am shubham");
+
 }
