754179a18588758127d003a995e002656d24c295
shubhamarman13 <shubhamarman13@gmail.com>
2024-01-12 22:51:45 +0530

    maths table updated

--- if_else.java
+++ if_else.java
@@ -1,4 +1,19 @@
+
+import java.util.Scanner;
 public class if_else {
-    
-    
+
+    public static void main(String[] args)
+    {
+        Scanner take = new Scanner(System.in);
+        System.out.println("Enter Your Age");
+        int age= take.nextInt();
+        if (age>18) {
+            System.out.println("Eliglible");
+            
+        }
+        else
+        {
+            System.out.println("Not Eliglible");
+        }
+    }
 }
---
+++ maths_table.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class maths_table {
+    public static void main(String[] args) {
+        System.out.println("Enter the Number to print the table \n");
+        Scanner take = new Scanner(System.in);
+        int num = take.nextInt();
+        for (int i = 1; i < 11; i++) {
+            System.out.println(num + " X " + num * i);
+        }
+    }
+
+}
